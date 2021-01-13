e8bd5378dcd2158581ebdc4578287ccffb5fc917
Stephanie Zablocki <sbz26@cornell.edu>
2021-01-13 12:02:26 -0500

    added sound

--- scripts/synth.js
+++ scripts/synth.js
@@ -5,12 +5,39 @@ var keyboard = new QwertyHancock({
     octaves: 2
 });
 
+var context = new AudioContext();
+masterVolume = context.createGain();
+
+masterVolume.gain.value = 0.3;
+masterVolume.connect(context.destination);
+
+var oscillators = {};
+
 keyboard.keyDown = function (note, frequency) {
-    console.log('Note', note, 'has been released');
-    console.log('Its frequency was', frequency);
+    var osc = context.createOscillator(),
+        osc2 = context.createOscillator();
+
+    osc.frequency.value = frequency;
+    osc.type = 'sawtooth';
+    osc.detune.value = -10;
+
+    osc2.frequency.value = frequency;
+    osc2.type = 'triangle';
+    osc2.detune.value = 10;
+
+    osc.connect(masterVolume);
+    osc2.connect(masterVolume);
+
+    masterVolume.connect(context.destination);
+
+    oscillators[frequency] = [osc, osc2];
+
+    osc.start(context.currentTime);
+    osc2.start(context.currentTime);
 };
 
 keyboard.keyUp = function (note, frequency) {
-    console.log('Note', note, 'has been released');
-    console.log('Its frequency was', frequency);
+    oscillators[frequency].forEach(function (oscillator) {
+        oscillator.stop(context.currentTime);
+    });
 }
